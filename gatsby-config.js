3da1880b7b32bf82ab0971495b6148c3983d3e2c
Joel Louzado <jlouzado@gmail.com>
2021-03-30 13:39:29 +0530

    feat(graphql): fetch data from github

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,27 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+const { buildClientSchema } = require('graphql')
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'GitHub',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        // HTTP headers
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          Authorization: `Bearer ${process.env.GITHUB_AUTH}`
+        }
+      }
+    }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return <div>Hello {data.github.user.name}!</div>
 }
+
+export const query = graphql`
+  query MyQuery {
+    github {
+      user(login: "jlouzado") {
+        bio
+        name
+        createdAt
+      }
+    }
+  }
+`
