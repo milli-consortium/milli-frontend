1f8ada98f1be6a5cd5a75084348ef3adeb627519
Joel Louzado <jlouzado@gmail.com>
2021-04-05 23:07:26 +0530

    build(graphql): generate gql typings from queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,34 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-graphql-codegen`,
+      options: {
+        documentPaths: [
+          './src/**/*.{ts,tsx}',
+          './.cache/fragments/*.ts',
+          './node_modules/gatsby-*/**/*.ts',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'Github',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        // HTTP headers
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          Authorization: `Bearer ${process.env.GITHUB_AUTH}`,
+        },
+      },
+    },
+  ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { PageProps } from 'gatsby';
-
+import { PageProps, graphql } from 'gatsby';
 import Title from '@/components/Title';
+import { GithubUserDataQuery } from '../../graphql-types';
 
-const Home: React.FC<PageProps> = () => (
+const Home: React.FC<PageProps<GithubUserDataQuery>> = ({ data }) => (
   <main>
     <Title />
     <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
@@ -11,7 +11,20 @@ const Home: React.FC<PageProps> = () => (
       Follow me on Twitter (
       <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
     </p>
+    <p>{JSON.stringify(data)}</p>
   </main>
 );
 
 export default Home;
+
+export const query = graphql`
+  query GithubUserData {
+    github {
+      user(login: "jlouzado") {
+        bio
+        name
+        createdAt
+      }
+    }
+  }
+`;
