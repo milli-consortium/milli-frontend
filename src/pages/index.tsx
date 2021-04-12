701a29dff47ce8efcd75a7ebe56848391d018a2e
Joel Louzado <jlouzado@gmail.com>
2021-04-12 17:32:15 +0530

    feat(schema.sdl): add query for entity search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,6 @@ import { NiosxDataQuery } from '../../graphql-types';
 const Home: React.FC<PageProps<NiosxDataQuery>> = ({ data }) => (
   <main>
     <Title />
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
     <p>{JSON.stringify(data)}</p>
   </main>
 );
@@ -20,10 +15,30 @@ export default Home;
 export const query = graphql`
   query NiosxData {
     niosx {
-      allCollections {
-        graphId
-        title
-        creator
+      searchCollections {
+        edges {
+          node {
+            graphId
+            title
+          }
+          cursor
+          offset
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+          filters {
+            blob
+            lang {
+              displayName
+              recordCount
+            }
+            subjects {
+              displayName
+              recordCount
+            }
+          }
+        }
       }
     }
   }
