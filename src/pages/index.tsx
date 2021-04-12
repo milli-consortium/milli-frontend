daa8b1a1ff8227c6de79d26d42e6eb28e9bf1ebc
Joel Louzado <jlouzado@gmail.com>
2021-04-12 20:24:58 +0530

    feat(index): add card details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,41 @@ import { PageProps, graphql } from 'gatsby';
 import Title from '@/components/Title';
 import { NiosxDataQuery } from '../../graphql-types';
 
+const keysOf = <T extends Record<string, unknown>>(o: T): Array<keyof T> =>
+  Object.keys(o);
+
 const Home: React.FC<PageProps<NiosxDataQuery>> = ({ data }) => (
   <main>
     <Title />
-    <p>{JSON.stringify(data)}</p>
+    <div>
+      <div>Header</div>
+      <div>Search Bar</div>
+      <div>
+        {data.niosx.searchCollections.pageInfo.filters
+          ? keysOf(data.niosx.searchCollections.pageInfo.filters).map((key) => {
+              const filter = data.niosx.searchCollections.pageInfo.filters[key];
+
+              return typeof filter === 'string' ? (
+                <div>{`Showing results for: ${filter}`}</div>
+              ) : (
+                <div>
+                  <h3>{key}</h3>
+                  {filter.map((f) => (
+                    <div key={f.id}>
+                      {f.displayName} ({f.recordCount ?? '--'})
+                    </div>
+                  ))}
+                </div>
+              );
+            })
+          : ''}
+      </div>
+    </div>
+    <div>
+      {data.niosx.searchCollections.edges.map((e) => (
+        <div>{JSON.stringify(e.node)}</div>
+      )) ?? <div>No Records Found</div>}
+    </div>
   </main>
 );
 
@@ -20,9 +51,19 @@ export const query = graphql`
           node {
             graphId
             title
+            partner
+            subjects
+            dateOfCreation
+            images {
+              src
+              alt
+              size
+            }
           }
           cursor
           offset
+          isDirectMatch
+          annotationMatchCount
         }
         pageInfo {
           endCursor
