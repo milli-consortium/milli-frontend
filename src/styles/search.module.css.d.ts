aeaf7fa6b2d046856a6620a838a08f78a184bbbc
Joel Louzado <jlouzado@gmail.com>
2021-04-20 00:02:49 +0530

    fix(search.css): add type declarations
    temporary stopgap till the module plugins are found

---
+++ src/styles/search.module.css.d.ts
@@ -0,0 +1,6 @@
+// TODO: generate these automatically.
+// Current Gatsby plugins appear to only be for v2
+
+export const container: string;
+export const filters: string;
+export const entities: string;
