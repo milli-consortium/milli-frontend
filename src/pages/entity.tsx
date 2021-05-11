8a5dab00c030e5b3d7a5c361d95c649601849ce1
Joel Louzado <jlouzado@gmail.com>
2021-05-11 16:20:57 +0530

    feat(entity): render entity details

--- src/pages/entity.tsx
+++ src/pages/entity.tsx
@@ -1,5 +1,23 @@
+import { entityQuery } from '@/queries/entity';
+import { EntityVariables, Entity_findEntity } from '@/queries/types/Entity';
+import { useQuery } from '@apollo/react-hooks';
 import React from 'react';
 
-export default function Enitity({ id }) {
-  return <div>Entity #{id}</div>;
+export default function Entity({ id }) {
+  const { loading, error, data } = useQuery<Entity_findEntity, EntityVariables>(
+    entityQuery,
+    {
+      variables: {
+        id,
+      },
+    },
+  );
+
+  return (
+    <div>
+      {loading && <div>Loading...</div>}
+      {!loading && data && <div>{JSON.stringify(data)}</div>}
+      {!loading && error && <div>Error: {error.message}</div>}
+    </div>
+  );
 }
---
+++ src/queries/entity.ts
@@ -0,0 +1,20 @@
+import { gql } from 'apollo-boost';
+
+export const entityQuery = gql`
+  query Entity($id: String!) {
+    findEntity(id: $id) {
+      graphId
+      agencyCode
+      title
+      partner
+      dateOfCreation
+      level
+      images {
+        src
+        alt
+        size
+      }
+      subjects
+    }
+  }
+`;
