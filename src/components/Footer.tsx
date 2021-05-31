0e950f1191acbdc5b0b05c64643618453855116d
Joel Louzado <jlouzado@gmail.com>
2021-06-01 00:45:28 +0530

    feat(footer): use mailto link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import { Flex } from 'antd-mobile';
-import * as styles from '../styles/search.module.css';
-import '../styles/search.css';
 import { Link } from 'gatsby';
-
-const gmail =
-  '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Gmail</title><path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/></svg>';
+import React from 'react';
+import '../styles/search.css';
+import * as styles from '../styles/search.module.css';
 
 export const Footer = () => (
   <div className="footerDiv margin-auto">
@@ -18,19 +15,19 @@ export const Footer = () => (
         </Flex.Item>
         <Flex.Item>
           <h4>Contact</h4>
-          <Link to="#">
-            <h3 className="linkColor">hello@milli.link</h3>
-          </Link>
+          <a href="mailto:hello@milli.link">
+            <h3 className="linkColor">hello [at] milli [dot] link</h3>
+          </a>
         </Flex.Item>
         <Flex.Item>
           <Link to="https://github.com/">
-            <div className="fleft github"></div>
+            <div className="fleft github" />
           </Link>
           <Link to="https://gmail.com/">
-            <div className="fleft gmail"></div>
+            <div className="fleft gmail" />
           </Link>
           <Link to="https://twitter.com/">
-            <div className="fleft twitter"></div>
+            <div className="fleft twitter" />
           </Link>
         </Flex.Item>
         <Flex.Item>
