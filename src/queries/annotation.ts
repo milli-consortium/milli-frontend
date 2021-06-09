fce7c0e7d4303f28e5521a10783e06d43abb53c4
salus-sage <salus.sage@gmail.com>
2021-06-09 17:48:47 +0530

    feat(add annotation mutation): add mutation in queries

---
+++ src/queries/annotation.ts
@@ -0,0 +1,41 @@
+import { gql } from 'apollo-boost';
+
+export const addAnnotation = gql`
+  mutation AnnotationInput(
+    $body: BodyInput!
+    $concept: String
+    $motivation: String!
+    $target: Target!
+    $targetId: String!
+  ) {
+    addAnnotation(
+      annotation: {
+        targetId: $targetId
+        target: $target
+        motivation: $motivation
+        concept: $concept
+        body: $body
+      }
+    ) {
+      graphId
+      context
+      id
+      type
+      motivation
+      body {
+        ... on TextualBody {
+          typ
+          purpose
+          value
+        }
+      }
+      target {
+        ... on SpecificResourceType {
+          typ
+          source
+          targetId
+        }
+      }
+    }
+  }
+`;
