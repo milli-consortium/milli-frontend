b3c3c691224b2caf1b7087afde0b2341a9f6427a
Joel Louzado <jlouzado@gmail.com>
2021-06-09 18:36:10 +0530

    feat(object-page): add addAnnotation mutation

--- apollo.config.js
+++ apollo.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   client: {
-    includes: [path.join(__dirname, '/src/queries/**/*.ts')],
+    includes: [
+      path.join(__dirname, '/src/queries/**/*.ts'),
+      path.join(__dirname, '/src/mutations/**/*.ts'),
+    ],
   },
 };
