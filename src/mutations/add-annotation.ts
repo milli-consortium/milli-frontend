fe7bc0e556c2c945d14a95e134ad019c291c6f15
Joel Louzado <jlouzado@gmail.com>
2021-06-09 20:27:29 +0530

    fix(addannotation): rename to include Mutation in the name

--- src/mutations/add-annotation.ts
+++ src/mutations/add-annotation.ts
@@ -1,6 +1,6 @@
 import { gql } from 'apollo-boost';
 
-export const addAnnotation = gql`
+export const addAnnotationMutation = gql`
   mutation AnnotationInput(
     $body: BodyInput!
     $concept: String
